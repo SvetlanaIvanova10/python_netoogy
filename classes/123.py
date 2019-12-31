e1d76042daea19ce8b23b20f65ca8b14b9f03104
SvetlanaIvanova10 <59331004+SvetlanaIvanova10@users.noreply.github.com>
2019-12-31 10:35:45 +0300

    Delete 123.py

--- classes/123.py
+++
@@ -1,9 +0,0 @@
-from python_netology.classes import Animals
-
-сlass Birds(Animals):
-    def __init__(self, eggs, name, weight, voices, food):
-        super().__init__(name, weight, voices, food)
-        self.eggs = eggs
-
-    def collect_eggs(self):
-        return "РЇРёС†Р° СЃРѕР±СЂР°РЅС‹" % self.eggs
