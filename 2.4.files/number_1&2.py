0b508c5fc1d8d0bf794d05357808f61450cee48c
SvetlanaIvanova10 <fascinatedsveta@gmail.com>
2020-01-28 22:39:30 +0300

    cook_book

--- 2.4.files/number_1&2.py
+++ 2.4.files/number_1&2.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 def dict_recipes():
     cook_book = {}
     for line in recipes:
@@ -16,34 +17,32 @@ def dict_recipes():
                 cook_book[key].append(ratings)
     return cook_book
 
-
-def out_cook_book(cook_book_):
-    print('cook_book = {')
-    for number in cook_book_.keys():
-        print(' "{}": [\n   {}\n   ],'.format(number, '\n   '.join(map(str, cook_book_[number]))))
-    print('}')
+def out_cook_book(cook_book):
+    print('cook_book =')
+    pprint(cook_book, indent=3, depth=3, width=150)
 
 def get_shop_list_by_dishes(dishes, person_count):
     cook_book_ingridients = dict_recipes()
     ingridients = {}
     for dis in dishes:
-        if dis  in cook_book_ingridients.keys():
+        if dis in cook_book_ingridients.keys():
             for ing in cook_book_ingridients[dis]:
                 keys = ing.get('ingredient_name')
                 count = {'measure': ing.get('measure'), 'quantity': int(ing.get('quantity'))*person_count}
                 if keys not in ingridients.keys():
-                    ingridients[keys] = list()
-                ingridients[keys].append(count)
+                    ingridients[keys] = count
+                else:
+                    new_count = {'measure': count.get('measure'), 'quantity': int(count.get('quantity')) * person_count}
+                    ingridients[keys] = new_count
         else:
             print('нет такого рецепта')
-    print("{")
-    for number in ingridients.keys():
-        print(' "{}": {},'.format(number, '\n   '.join(map(str, ingridients[number]))))
-    print("}")
+    pprint(ingridients)
 
 if __name__ == '__main__':
+    # Я сделала в разных with, потому что,если запустить в одном, то выполняется правильно только первая функция
+    #
+
     with open("recipes.txt", encoding='utf8') as recipes:
         out_cook_book(dict_recipes())
-    print()
-    with open("recipes.txt", encoding='utf8') as recipes:
-        get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
+        recipes.seek(0)
+        get_shop_list_by_dishes(['Омлет', 'Омлет'], 2)
