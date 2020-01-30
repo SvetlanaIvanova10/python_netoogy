feaa15943c82cf65679ddaee51f487cd4e98c81f
SvetlanaIvanova10 <fascinatedsveta@gmail.com>
2020-01-30 17:38:12 +0300

    json&xml

--- json&xml/json_file.py
+++ json&xml/json_file.py
@@ -7,8 +7,12 @@ news_description = []
 for item in news:
     description = item['description'].split(' ')
     news_description.extend(description)
-news_description = list(filter(lambda del_len: len(del_len) < 7, news_description))
-news_description.sort(key = len, reverse=True)
-occurence_count = Counter(news_description)
+news_description_lower = []
+for low in news_description:
+    news_description_lower.append(low.lower())
+news_description_lower_filter = list(filter(lambda del_len: len(del_len) >= 6, news_description_lower))
+news_description_lower_filter.sort(key = len, reverse=True)
+occurence_count = Counter(news_description_lower_filter)
 ten_count = occurence_count.most_common(10)
 print(ten_count)
+
--- json&xml/xml_file.py
+++ json&xml/xml_file.py
@@ -5,13 +5,15 @@ tree = ET.parse("newsafr.xml", parser)
 news_description = []
 root = tree.getroot()
 xml_items = root.findall("channel/item")
-
 for item in xml_items:
     description = item.find("description").text.split(" ")
     news_description.extend(description)
-
-news_description = list(filter(lambda del_len: len(del_len) < 7, news_description))
-news_description.sort(key=len, reverse=True)
-occurence_count = Counter(news_description)
+news_description_lower = []
+for low in news_description:
+    news_description_lower.append(low.lower())
+news_description_lower_filter = list(filter(lambda del_len: len(del_len) >= 6, news_description_lower))
+news_description_lower_filter.sort(key = len, reverse=True)
+occurence_count = Counter(news_description_lower_filter)
 ten_count = occurence_count.most_common(10)
 print(ten_count)
+
