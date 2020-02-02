e84b60661eaad489ac5562032f26a22d3c9b94e3
SvetlanaIvanova10 <fascinatedsveta@gmail.com>
2020-02-02 16:49:10 +0300

    API translate

--- requests,http-запросы/example.py
+++ requests,http-запросы/example.py
@@ -14,10 +14,11 @@ key = getenv('SECRET_KEY')
 API_KEY = 'trnsl.1.1.20190712T081241Z.0309348472c8719d.0efdbc7ba1c507292080e3fbffe4427f7ce9a9f0'
 URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
 
-def translate_it(path_file, path_result, from_lang):
+
+def translate_it(path_file, path_result, from_lang, to_lang='ru'):
     with open(path_file, encoding="utf8") as file:
         text = file.read()
-    lang = f'{from_lang}-ru'
+    lang = f'{from_lang}-{to_lang}'
     params = {
         'key': API_KEY,
         'text': text,
@@ -30,8 +31,8 @@ def translate_it(path_file, path_result, from_lang):
         result_file.write(result)
     return result_file
 
+
 if __name__ == '__main__':
     translate_it('DE.txt', 'translate_DE.txt', 'de')
     translate_it('ES.txt', 'translate_ES.txt', 'es')
     translate_it('FR.txt', 'translate_FR.txt', 'fr')
-
