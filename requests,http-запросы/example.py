0220aaffc5dd4114c2f875bc53012e3230bba223
SvetlanaIvanova10 <fascinatedsveta@gmail.com>
2020-02-02 16:31:10 +0300

    API translate

--- requests,http-запросы/example.py
+++ requests,http-запросы/example.py
@@ -8,13 +8,16 @@
 # У вас есть 3 файла (DE.txt, ES.txt, FR.txt) с новостями на 3 языках: французском, испанском, немецком.
 # Функция должна взять каждый файл с текстом, перевести его на русский и сохранить результат в новом файле.
 import requests
+from os import getenv
+key = getenv('SECRET_KEY')
+
 API_KEY = 'trnsl.1.1.20190712T081241Z.0309348472c8719d.0efdbc7ba1c507292080e3fbffe4427f7ce9a9f0'
 URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
 
-def translate_it(path_file, path_result, from_lang, to_lang):
+def translate_it(path_file, path_result, from_lang):
     with open(path_file, encoding="utf8") as file:
         text = file.read()
-    lang = f'{from_lang}-{to_lang}'
+    lang = f'{from_lang}-ru'
     params = {
         'key': API_KEY,
         'text': text,
@@ -28,7 +31,7 @@ def translate_it(path_file, path_result, from_lang, to_lang):
     return result_file
 
 if __name__ == '__main__':
-    translate_it('DE.txt', 'translate_DE.txt', 'de', 'ru')
-    translate_it('ES.txt', 'translate_ES.txt', 'es', 'ru')
-    translate_it('FR.txt', 'translate_FR.txt', 'fr', 'ru')
+    translate_it('DE.txt', 'translate_DE.txt', 'de')
+    translate_it('ES.txt', 'translate_ES.txt', 'es')
+    translate_it('FR.txt', 'translate_FR.txt', 'fr')
 
