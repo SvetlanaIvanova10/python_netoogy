c9b3cc1630fccb45db9740e7b30dd4d43dd255f6
SvetlanaIvanova10 <fascinatedsveta@gmail.com>
2020-02-02 17:12:50 +0300

    API translate

--- requests,http-запросы/example.py
+++ requests,http-запросы/example.py
@@ -9,9 +9,10 @@
 # Функция должна взять каждый файл с текстом, перевести его на русский и сохранить результат в новом файле.
 import requests
 from os import getenv
-key = getenv('SECRET_KEY')
 
+API_KEY = getenv('SECRET_KEY')
 API_KEY = 'trnsl.1.1.20190712T081241Z.0309348472c8719d.0efdbc7ba1c507292080e3fbffe4427f7ce9a9f0'
+
 URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
 
 
