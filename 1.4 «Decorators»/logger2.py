0a4b95d25f49d426c0615f3b4070beab7d7a0173
SvetlanaIvanova10 <fascinatedsveta@gmail.com>
2020-03-26 09:25:04 +0300

    param_decorator

--- 1.4 «Decorators»/logger2.py
+++ 1.4 «Decorators»/logger2.py
@@ -2,25 +2,24 @@
 import logging
 from functools import wraps
 
-
-def info_decorator(func):
-    @wraps(func)
-    def info_about_func(*argv, **kwargv):
-        my_file = input('введите название файла для записи логгера (без формата): ')
-        format = f'%(levelname)s [%(asctime)s] %(message)s'
-        logging.basicConfig(format = format,
-                            level = logging.DEBUG,
-                            filename = f'{my_file}.log',
-                            filemode = 'w')
-        result = func(*argv, **kwargv)
-        logging.info(
-            f"имя функции: {func.__name__}, аргументы: {argv},{kwargv}, возвращаемое значение: {result}")
-        return func(*argv, **kwargv)
-    return info_about_func
-
+def param_info_decorator(name_file):
+    def info_decorator(func):
+        @wraps(func)
+        def info_about_func(*argv, **kwargv):
+            format = f'%(levelname)s [%(asctime)s] %(message)s'
+            logging.basicConfig(format = format,
+                                level = logging.DEBUG,
+                                filename = name_file,
+                                filemode = 'w')
+            result = func(*argv, **kwargv)
+            logging.info(
+                f"имя функции: {func.__name__}, аргументы: {argv},{kwargv}, возвращаемое значение: {result}")
+            return func(*argv, **kwargv)
+        return info_about_func
+    return info_decorator
 
 if __name__ == '__main__':
-    @info_decorator
+    @param_info_decorator('log_file.log')
     def plus(a, b):
         result = a + b
         return result
